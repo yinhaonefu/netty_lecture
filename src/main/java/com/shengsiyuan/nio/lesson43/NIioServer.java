c6c5194e0d78b51d1d0ef27c17a8280b6a1b68a2
yinhao-mac <yinhao@jd.com>
2017-10-31 23:06:44 +0800

    lesson44

---
+++ src/main/java/com/shengsiyuan/nio/lesson43/NIioServer.java
@@ -0,0 +1,91 @@
+package com.shengsiyuan.nio.lesson43;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by yinhao on 17/10/31.
+ * 启动程序，在多个终端分别使用nc 或者telnet localhost 8899 连接 输入内容 查看输出
+ */
+public class NIioServer {
+
+    public static Map<String, SocketChannel> clientMap = new HashMap<>();
+
+    public static void main(String[] args)throws Exception {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(new InetSocketAddress(8899));
+
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        while (true){
+            try {
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+                selectionKeys.forEach(selectionKey -> {
+                    final SocketChannel client;
+                    try {
+                        if(selectionKey.isAcceptable()){
+                            ServerSocketChannel server = (ServerSocketChannel) selectionKey.channel();
+                            client = server.accept();
+                            client.configureBlocking(false);
+                            client.register(selector,SelectionKey.OP_READ);
+
+                            String key = "["+ UUID.randomUUID()+"]";
+                            clientMap.put(key,client);
+
+                        }else if (selectionKey.isReadable()){
+                            client = (SocketChannel) selectionKey.channel();
+                            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                            int count = client.read(byteBuffer);
+                            if (count > 0){
+                                byteBuffer.flip();
+
+                                Charset charset = Charset.forName("utf-8");
+                                String receiveMessage = String.valueOf(charset.decode(byteBuffer).array());
+                                System.out.println(client+":"+receiveMessage);
+
+                                //获取发送消息段的channel对应的key，用于通知其他channel是谁发的消息
+                                String senderKey = null;
+                                for (Map.Entry<String,SocketChannel> entry : clientMap.entrySet()){
+                                    if (client == entry.getValue()){
+                                        senderKey = entry.getKey();
+                                        break;
+                                    }
+                                }
+
+                                //向每一个注册的的channel发送接收到的内容以及，该内容的发送者
+                                for (Map.Entry<String,SocketChannel> entry : clientMap.entrySet()){
+                                    SocketChannel value = entry.getValue();
+                                    ByteBuffer buffer = ByteBuffer.allocate(1024);
+                                    buffer.put((senderKey+":"+receiveMessage).getBytes());
+                                    buffer.flip();
+                                    value.write(buffer);
+                                }
+                            }
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                });
+
+                selectionKeys.clear();//不删除之前注册的channel会引起空指针异常
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/shengsiyuan/nio/lesson43/NioClient.java
@@ -0,0 +1,76 @@
+package com.shengsiyuan.nio.lesson43;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by yinhao on 17/10/31.
+ */
+public class NioClient {
+    public static void main(String[] args)throws Exception {
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            Selector selector = Selector.open();
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+            socketChannel.connect(new InetSocketAddress("127.0.0.1",8899));
+
+            while (true){
+                selector.select();//阻塞等待注册的channel，服务端向客户端返回
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                for (SelectionKey selectionKey : selectionKeys){
+                    if (selectionKey.isConnectable()){
+                        SocketChannel client = (SocketChannel) selectionKey.channel();
+                        if (client.isConnectionPending()){
+                            client.finishConnect();//连接建立完毕
+                            //通知服务器连接成功
+                            ByteBuffer buffer = ByteBuffer.allocate(1024);
+                            buffer.put((LocalDateTime.now()+"连接成功").getBytes());
+                            buffer.flip();
+                            client.write(buffer);
+
+                            //连接成功后，新启动一个线程，不断读取用户输入发送给服务端
+                            ExecutorService executorService = Executors.newSingleThreadExecutor(Executors.defaultThreadFactory());
+                            executorService.submit(() -> {
+                                while (true){
+                                    try {
+                                        buffer.clear();
+                                        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+                                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                                        String readLine = bufferedReader.readLine();
+                                        buffer.put(readLine.getBytes());
+                                        buffer.flip();
+                                        client.write(buffer);
+                                    }catch (Exception e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                        }
+                        client.register(selector,SelectionKey.OP_READ);
+                    }else if (selectionKey.isReadable()){
+                        SocketChannel client = (SocketChannel) selectionKey.channel();
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                        int count = client.read(byteBuffer);
+                        if (count > 0){
+                            String receiveMessage = new String(byteBuffer.array(),0,count);
+                            System.out.println(receiveMessage);
+                        }
+                    }
+                }
+                selectionKeys.clear();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
