29a6ba773e002e027bf427c1ada603f3385bae16
yinhao <yinhao@360buyAD.local>
2018-02-24 16:02:16 +0800

    lesson39

--- src/main/java/com/shengsiyuan/nio/NIOTest11.java
+++ src/main/java/com/shengsiyuan/nio/NIOTest11.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
  * 应用场景
  * 如果自定义协议，可以将header中数据放到第一个Buffer中，将body放到第一个Buffer中，很自然的将报文各部分隔离
  *
- * 运行下面程序，然后可以通过telnet localhost 8899 或者 nc localhost 8899
+ * 运行下面程序，然后可以通过telnet localhost 8899 或者 nc localhost 8899 （mac自带）
  * 输入内容查看输出，只有全部的Buffer都读满时，才会回写所有读到的内容
  */
 public class NIOTest11 {
@@ -35,13 +35,13 @@ public class NIOTest11 {
         buffers[1] = ByteBuffer.allocate(3);
         buffers[2] = ByteBuffer.allocate(4);
 
-        SocketChannel socketChannel = serverSocketChannel.accept();
+        SocketChannel socketChannel = serverSocketChannel.accept();//等待客户端连接
 
         while (true){
             int bytesRead = 0;
 
             while (bytesRead < messageLength){
-                long r = socketChannel.read(buffers);
+                long r = socketChannel.read(buffers);//等待客户端输入
                 bytesRead += r;
                 System.out.println("bytesRead:"+bytesRead);
                 Arrays.asList(buffers).stream().
@@ -54,11 +54,13 @@ public class NIOTest11 {
             long bytesWriten = 0;
 
             while (bytesWriten < messageLength) {
-                long r = socketChannel.write(buffers);
+                long r = socketChannel.write(buffers);//将客户端的输入回写给客户端
                 bytesWriten += r;
             }
 
             Arrays.asList(buffers).forEach(byteBuffer -> byteBuffer.clear());
+
+            System.out.println("bytesRead: " + bytesRead + ", bytesWriten: " + bytesWriten + ", messageLength: " + messageLength);
         }
 
     }
--- src/main/java/com/shengsiyuan/nio/NIOTest9.java
+++ src/main/java/com/shengsiyuan/nio/NIOTest9.java
@@ -13,6 +13,9 @@ public class NIOTest9 {
     public static void main(String[] args)throws Exception {
         RandomAccessFile randomAccessFile = new RandomAccessFile("NioTest9.txt","rw");
         FileChannel fileChannel = randomAccessFile.getChannel();
+        //映射文件为读写模式，并映射从第0个位置开始一共5个字节的内容到操作系统内存中
+        //直接在操作系统内存中修改对应位置的字节，文件内容也发生变化
+        //idea打开看不出变化，需要到文件目录中单独打开查看
         MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE,0,5);
         mappedByteBuffer.put(0,(byte)'a');
         mappedByteBuffer.put(3,(byte)'b');
